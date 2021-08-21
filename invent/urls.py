6f745d675e31272f0ff44af1702b4f25d0d428c4
bwaargh <rawpluganchor@gmail.com>
2021-08-20 20:53:20 -0500

    save

--- invent/urls.py
+++ invent/urls.py
@@ -1,24 +1,29 @@
 from django.urls import path
 
-from . import views
+from .views import (
+    index,
+    SummaryListView,
+    TruckFormSetView,
+    TrailerFormSetView,
+    TruckDetailView,
+    TrailerDetailView,
+)
 
 app_name = "invent"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('summary/', views.SummaryListView.as_view(
+    path('', index, name='index'),
+    path('summary/', SummaryListView.as_view(
         template_name="invent/summary.html"),
         name='summary'),
-    # path('trucks/list/', views.trucks_list_view,
-    #      name='list_trucks'),
-    path('trucks/list/', views.TruckFormSetView.as_view(),
+    path('trucks/list/', TruckFormSetView.as_view(),
          name='list_trucks'),
-    path('trailers/list/', views.TrailerFormSetView.as_view(),
+    path('trailers/list/', TrailerFormSetView.as_view(),
          name='list_trailers'),
-    path('truck/<int:pk>', views.TruckDetailView.as_view(
+    path('truck/<int:pk>', TruckDetailView.as_view(
         template_name="invent/detail.html"),
         name='truck'),
-    path('trailer/<int:pk>', views.TrailerDetailView.as_view(
+    path('trailer/<int:pk>', TrailerDetailView.as_view(
         template_name="invent/detail.html"),
         name='trailer'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from . import views
 from .forms import UserLoginForm
 from .utils import generate_su_profile
+from .views import (
+    register,
+    PreferenceListUpdateView,
+    ListColShowListView,
+    UsersLevelFormSetView,
+)
 
 app_name = "users"
 
@@ -14,16 +19,14 @@ urlpatterns = [
         name="login"),
     path('logout/', LogoutView.as_view(next_page='invent:index'),
          name="logout"),
-    path('register/', views.register, name="register"),
+    path('register/', register, name="register"),
     path('generate_profile/', generate_su_profile, name="generate_su_profile"),
-    path('user/settings/<int:pk>', views.PreferenceListUpdateView.as_view(
+    path('user/settings/<int:pk>', PreferenceListUpdateView.as_view(
         template_name='users/preferences.html'),
         name="preferences"),
-    path('user/settings/table_columns', views.ListColShowListView.as_view(
+    path('user/settings/table_columns', ListColShowListView.as_view(
         template_name='users/listcolshow.html'),
         name="listcolshow"),
-    # path('account/users/', views.users_level_view,
-    #     name="update_level"),
-    path('account/users/', views.UsersLevelFormSetView.as_view(),
-        name="update_level"),
+    path('account/users/', UsersLevelFormSetView.as_view(),
+         name="update_level"),
 ]
