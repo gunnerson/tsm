c43f9593cd2ed28a123e68de250f295f0b8ef2cc
fleetwood <fleetwoodrepair@gmail.com>
2021-10-21 13:32:48 -0500

    save

--- docs/models.py
+++ docs/models.py
@@ -35,7 +35,7 @@ class Document(models.Model):
         choices=file_category_choices(),
         default="GN",
     )
-    description = models.CharField(max_length=50, default=True)
+    description = models.CharField(max_length=50, blank=True)
 
     class Meta:
         abstract = True
