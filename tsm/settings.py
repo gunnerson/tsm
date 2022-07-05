61719a8cb403bd2caa8f90a48605a97f3d88b16c
Debian <admin@ip-172-31-22-234.us-east-2.compute.internal>
2022-07-05 18:04:25 +0000

    fix

--- tsm/settings.py
+++ tsm/settings.py
@@ -20,14 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('TSM_SECRET_KEY')
-
+#SECRET_KEY = os.environ.get('TSM_SECRET_KEY')
+SECRET_KEY = "dsvcsdvsdbvbfb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = True if os.environ.get('DJANGO_DEBUG') == 'true' else False
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_HOSTS', '127.0.0.1').split(',')
+#ALLOWED_HOSTS = os.environ.get('DJANGO_HOSTS').split(',')
+ALLOWED_HOSTS = ['tsm-env.eba-zd8tvpe3.us-east-2.elasticbeanstalk.com',]
 
 # Application definition
 INSTALLED_APPS = [
