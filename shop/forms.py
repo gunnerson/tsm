6401a7a839e0ce16a48efa882a043b68deb962d5
viktor <rawpluganchor@gmail.com>
2022-08-02 13:34:24 -0500

    save

--- shop/forms.py
+++ shop/forms.py
@@ -82,6 +82,7 @@ class OrderPartForm(forms.ModelForm):
             self.fields["part"].disabled = True
         for f in self.fields:
             self.fields[f].widget.attrs.update({'class': 'form_field'})
+            print(self.fields[f].initial)
 
 
 class PartTypeForm(FormSetMixin):
