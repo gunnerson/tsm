b80e894041b21a0dfaed60e5c5b6a7241c73ca61
viktor <rawpluganchor@gmail.com>
2022-08-02 13:31:37 -0500

    save

--- shop/forms.py
+++ shop/forms.py
@@ -78,7 +78,7 @@ class OrderPartForm(forms.ModelForm):
             queryset=parts,
             widget=OrderSelect(exclude=exclude),
         )
-        if self.fields["part"] is not "---------":
+        if self.fields["part"].initial != "---------":
             self.fields["part"].disabled = True
         for f in self.fields:
             self.fields[f].widget.attrs.update({'class': 'form_field'})
